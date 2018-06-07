b3a06d11e61309ae8847d845b3f3ad7c9a6c26b8
vinaylamba9 <35932009+vinaylamba9@users.noreply.github.com>
2018-06-07 12:07:22 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var fname = prompt("What is your first name");
+var lname = prompt("What is your last name");
+var age= prompt("What is your age");
+var days = age*365.25;
+alert("Your name is " + fname +" " +lname+"."+"\nYour age is " + age +" years."+"\nYou have been roughly alive from "+ days);
+console.log("Your name is " + fname +" " +lname+".");
+console.log("Your age is " + age +" years.");
